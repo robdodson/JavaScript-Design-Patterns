cac9359fcafe1b2c90bb4514922b7f7e6b62f93a
Rob Dodson <lets.email.rob@gmail.com>
2012-08-29 09:30:31 -0700

    Add Singleton Example

---
+++ singleton/namespace/main.js
@@ -0,0 +1,11 @@
+var NAMESPACE = {};
+
+NAMESPACE.Widget = function(foo, bar) {
+  // Some awesome widget code
+};
+
+NAMESPACE.Widget.prototype.doSomethingAwesome = function() {
+  // do something awesome!!!
+};
+
+var myWidget = new NAMESPACE.Widget('hello', 'world');
